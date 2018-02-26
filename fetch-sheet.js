91d0afd525f9f064a2fcaf9a367ebe31b04c3d80
Prasanth <prasanthv@ibtspl.com>
2018-02-26 18:32:48 +0530

    date validation, utf8 strings handled

--- fetch-sheet.js
+++ fetch-sheet.js
@@ -1,63 +1,119 @@
 'use strict';
 
+var _regenerator = require('babel-runtime/regenerator');
+
+var _regenerator2 = _interopRequireDefault(_regenerator);
+
 var _asyncToGenerator2 = require('babel-runtime/helpers/asyncToGenerator');
 
 var _asyncToGenerator3 = _interopRequireDefault(_asyncToGenerator2);
 
 function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
 
-const GoogleSpreadsheet = require('google-spreadsheet');
-const _ = require('lodash');
+var GoogleSpreadsheet = require('google-spreadsheet');
+var _ = require('lodash');
+var isValid = require('date-fns/is_valid');
 
-const getSpreadsheet = (spreadsheetId, credentials) => new Promise((resolve, reject) => {
-    const doc = new GoogleSpreadsheet(spreadsheetId);
+var getSpreadsheet = function getSpreadsheet(spreadsheetId, credentials) {
+  return new Promise(function (resolve, reject) {
+    var doc = new GoogleSpreadsheet(spreadsheetId);
     doc.useServiceAccountAuth(credentials, function (err) {
-        if (err) reject(err);else resolve(doc);
+      if (err) reject(err);else resolve(doc);
     });
-});
+  });
+};
 
-const getWorksheetByTitle = (spreadsheet, worksheetTitle) => new Promise((resolve, reject) => spreadsheet.getInfo((e, s) => {
-    if (e) reject(e);
-    const targetSheet = s.worksheets.find(sheet => sheet.title === worksheetTitle);
-    if (!targetSheet) {
+var getWorksheetByTitle = function getWorksheetByTitle(spreadsheet, worksheetTitle) {
+  return new Promise(function (resolve, reject) {
+    return spreadsheet.getInfo(function (e, s) {
+      if (e) reject(e);
+      var targetSheet = s.worksheets.find(function (sheet) {
+        return sheet.title === worksheetTitle;
+      });
+      if (!targetSheet) {
         reject(`Found no worksheet with the title ${worksheetTitle}`);
-    }
-    resolve(targetSheet);
-}));
-
-const getRows = (worksheet, options = {}) => new Promise((resolve, reject) => worksheet.getRows(options, (err, rows) => {
-    if (err) reject(err);else {
-        resolve(rows.map(r =>
-        // google sheets mangles column names, so we use a dash-lowercase convention
-        // , and now we make that JS friendly w/ camelcase
-        _.mapKeys(
-        // system values we don't need
-        _.omit(r, ['_xml', 'app:edited', 'save', 'del', '_links']), (val, key) => _.camelCase(key))));
-    }
-}));
-
-const cleanRows = rows => rows.map(r => _.mapValues(r, val => {
-    if (val === '') return null;
-    // sheets apparently leaves commas in some #s depending on formatting
-    if (!isNaN(val.replace(/[\W]/g, '')) && val !== '') {
-        return Number(val.replace(/[\W]/g, ''));
-    }
-    if (val === 'TRUE') return true;
-    if (val === 'FALSE') return false;
-    return val;
-}));
-
-const fetchData = (() => {
-    var _ref = (0, _asyncToGenerator3.default)(function* (spreadsheetId, worksheetTitle, credentials) {
-        const spreadsheet = yield getSpreadsheet(spreadsheetId, credentials);
-        const worksheet = yield getWorksheetByTitle(spreadsheet, worksheetTitle);
-        const rows = yield getRows(worksheet);
-        return cleanRows(rows);
+      }
+      resolve(targetSheet);
     });
+  });
+};
 
-    return function fetchData(_x, _x2, _x3) {
-        return _ref.apply(this, arguments);
-    };
-})();
+var getRows = function getRows(worksheet) {
+  var options = arguments.length > 1 && arguments[1] !== undefined ? arguments[1] : {};
+  return new Promise(function (resolve, reject) {
+    return worksheet.getRows(options, function (err, rows) {
+      if (err) reject(err);else {
+        resolve(rows.map(function (r) {
+          return (
+            // google sheets mangles column names, so we use a dash-lowercase convention
+            // , and now we make that JS friendly w/ camelcase
+            _.mapKeys(
+            // system values we don't need
+            _.omit(r, ['_xml', 'app:edited', 'save', 'del', '_links']), function (val, key) {
+              return _.camelCase(key);
+            })
+          );
+        }));
+      }
+    });
+  });
+};
+
+var cleanRows = function cleanRows(rows) {
+  return rows.map(function (r) {
+    return _.mapValues(r, function (val, key) {
+      if (key === 'date') {
+        if (val !== '' && isValid(new Date(val))) {
+          return unescape(encodeURIComponent(new Date(val)));
+        }
+      }
+      if (val === '') return null;
+      // sheets apparently leaves commas in some #s depending on formatting
+      if (!isNaN(val.replace(/[\W]/g, '')) && val.replace(/[\W]/g, '') !== '' && val !== '') {
+        return unescape(encodeURIComponent(Number(val.replace(/[\W]/g, ''))));
+      }
+      if (val === 'TRUE') return unescape(encodeURIComponent('true'));
+      if (val === 'FALSE') return unescape(encodeURIComponent('false'));
+      return unescape(encodeURIComponent(val));
+    });
+  });
+};
+
+var fetchData = function () {
+  var _ref = (0, _asyncToGenerator3.default)( /*#__PURE__*/_regenerator2.default.mark(function _callee(spreadsheetId, worksheetTitle, credentials) {
+    var spreadsheet, worksheet, rows;
+    return _regenerator2.default.wrap(function _callee$(_context) {
+      while (1) {
+        switch (_context.prev = _context.next) {
+          case 0:
+            _context.next = 2;
+            return getSpreadsheet(spreadsheetId, credentials);
+
+          case 2:
+            spreadsheet = _context.sent;
+            _context.next = 5;
+            return getWorksheetByTitle(spreadsheet, worksheetTitle);
+
+          case 5:
+            worksheet = _context.sent;
+            _context.next = 8;
+            return getRows(worksheet);
+
+          case 8:
+            rows = _context.sent;
+            return _context.abrupt('return', cleanRows(rows));
+
+          case 10:
+          case 'end':
+            return _context.stop();
+        }
+      }
+    }, _callee, undefined);
+  }));
+
+  return function fetchData(_x2, _x3, _x4) {
+    return _ref.apply(this, arguments);
+  };
+}();
 
 module.exports = fetchData;
--- src/fetch-sheet.js
+++ src/fetch-sheet.js
@@ -1,65 +1,73 @@
 // @flow
 const GoogleSpreadsheet = require('google-spreadsheet')
 const _ = require('lodash')
+var isValid = require('date-fns/is_valid')
 
 const getSpreadsheet = (spreadsheetId, credentials) =>
-    new Promise((resolve, reject) => {
-      const doc = new GoogleSpreadsheet(spreadsheetId)
-      doc.useServiceAccountAuth(credentials, function (err) {
-        if (err) reject(err)
-        else resolve(doc)
-      })
+  new Promise((resolve, reject) => {
+    const doc = new GoogleSpreadsheet(spreadsheetId)
+    doc.useServiceAccountAuth(credentials, function(err) {
+      if (err) reject(err)
+      else resolve(doc)
     })
+  })
 
 const getWorksheetByTitle = (spreadsheet, worksheetTitle) =>
-    new Promise((resolve, reject) =>
-        spreadsheet.getInfo((e, s) => {
-          if (e) reject(e)
-          const targetSheet = s.worksheets.find(
-                sheet => sheet.title === worksheetTitle
-            )
-          if (!targetSheet) { reject(`Found no worksheet with the title ${worksheetTitle}`) }
-          resolve(targetSheet)
-        })
-    )
+  new Promise((resolve, reject) =>
+    spreadsheet.getInfo((e, s) => {
+      if (e) reject(e)
+      const targetSheet = s.worksheets.find(
+        sheet => sheet.title === worksheetTitle
+      )
+      if (!targetSheet) {
+        reject(`Found no worksheet with the title ${worksheetTitle}`)
+      }
+      resolve(targetSheet)
+    })
+  )
 
 const getRows = (worksheet, options = {}) =>
-    new Promise((resolve, reject) =>
-        worksheet.getRows(options, (err, rows) => {
-          if (err) reject(err)
-          else {
-            resolve(
-                    rows.map(r =>
-                        // google sheets mangles column names, so we use a dash-lowercase convention
-                        // , and now we make that JS friendly w/ camelcase
-                        _.mapKeys(
-                            // system values we don't need
-                            _.omit(r, [
-                              '_xml',
-                              'app:edited',
-                              'save',
-                              'del',
-                              '_links'
-                            ]),
-                            (val, key) => _.camelCase(key)
-                        )
-                    )
-                )
-          }
-        })
-    )
+  new Promise((resolve, reject) =>
+    worksheet.getRows(options, (err, rows) => {
+      if (err) reject(err)
+      else {
+        resolve(
+          rows.map(r =>
+            // google sheets mangles column names, so we use a dash-lowercase convention
+            // , and now we make that JS friendly w/ camelcase
+            _.mapKeys(
+              // system values we don't need
+              _.omit(r, ['_xml', 'app:edited', 'save', 'del', '_links']),
+              (val, key) => _.camelCase(key)
+            )
+          )
+        )
+      }
+    })
+  )
 
 const cleanRows = rows =>
-    rows.map(r =>
-        _.mapValues(r, val => {
-          if (val === '') return null
-            // sheets apparently leaves commas in some #s depending on formatting
-          if (!isNaN(val.replace(/[\W]/g, '')) && val !== '') { return Number(val.replace(/[\W]/g, '')) }
-          if (val === 'TRUE') return true
-          if (val === 'FALSE') return false
-          return val
-        })
-    )
+  rows.map(r =>
+    _.mapValues(r, (val, key) => {
+      if (key === 'date') {
+        if (val !== '' && isValid(new Date(val))) {
+          return unescape(encodeURIComponent(new Date(val)))
+        }
+      }
+      if (val === '') return null
+      // sheets apparently leaves commas in some #s depending on formatting
+      if (
+        !isNaN(val.replace(/[\W]/g, '')) &&
+        val.replace(/[\W]/g, '') !== '' &&
+        val !== ''
+      ) {
+        return unescape(encodeURIComponent(Number(val.replace(/[\W]/g, ''))))
+      }
+      if (val === 'TRUE') return unescape(encodeURIComponent('true'))
+      if (val === 'FALSE') return unescape(encodeURIComponent('false'))
+      return unescape(encodeURIComponent(val))
+    })
+  )
 
 const fetchData = async (spreadsheetId, worksheetTitle, credentials) => {
   const spreadsheet = await getSpreadsheet(spreadsheetId, credentials)
