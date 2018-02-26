0de09512550192820904f451f61b88438d0e134d
brandonmp <brandon.m.paquette@gmail.com>
2018-02-26 09:34:25 -0500

    added prettier & accompanying npm script

--- src/gatsby-node.js
+++ src/gatsby-node.js
@@ -1,19 +1,19 @@
-const fetchSheet = require(`./fetch-sheet.js`)
-const uuidv5 = require('uuid/v5')
-const _ = require('lodash')
-const crypto = require('crypto')
-const seedConstant = '2972963f-2fcf-4567-9237-c09a2b436541'
+const fetchSheet = require(`./fetch-sheet.js`);
+const uuidv5 = require("uuid/v5");
+const _ = require("lodash");
+const crypto = require("crypto");
+const seedConstant = "2972963f-2fcf-4567-9237-c09a2b436541";
 
 exports.sourceNodes = async (
-    { boundActionCreators, getNode, store, cache },
-    { spreadsheetId, worksheetTitle, credentials }
+  { boundActionCreators, getNode, store, cache },
+  { spreadsheetId, worksheetTitle, credentials }
 ) => {
-  const { createNode } = boundActionCreators
+  const { createNode } = boundActionCreators;
 
-  let rows = await fetchSheet(spreadsheetId, worksheetTitle, credentials)
+  let rows = await fetchSheet(spreadsheetId, worksheetTitle, credentials);
 
   rows.forEach(r => {
-        /* console.log(
+    /* console.log(
             _.mapValues(r, (val, key) => ({
                 isNull: _.isNull(val),
                 isNumber: _.isFinite(val),
@@ -21,18 +21,18 @@ exports.sourceNodes = async (
             }))
         ); */
     createNode(
-            Object.assign(r, {
-              id: uuidv5(r.id, uuidv5('gsheet', seedConstant)),
-              parent: '__SOURCE__',
-              children: [],
-              internal: {
-                type: _.camelCase(`googleSheet ${worksheetTitle} row`),
-                contentDigest: crypto
-                        .createHash('md5')
-                        .update(JSON.stringify(r))
-                        .digest('hex')
-              }
-            })
-        )
-  })
-}
+      Object.assign(r, {
+        id: uuidv5(r.id, uuidv5("gsheet", seedConstant)),
+        parent: "__SOURCE__",
+        children: [],
+        internal: {
+          type: _.camelCase(`googleSheet ${worksheetTitle} row`),
+          contentDigest: crypto
+            .createHash("md5")
+            .update(JSON.stringify(r))
+            .digest("hex")
+        }
+      })
+    );
+  });
+};
