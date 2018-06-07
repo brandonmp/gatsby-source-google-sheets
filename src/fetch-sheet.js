797e95c720e7c8f9c69a4fc97c712b2ee9d95e68
Manel García <manelet@gmail.com>
2018-06-07 19:37:18 +0200

    allowing public sheets

--- src/fetch-sheet.js
+++ src/fetch-sheet.js
@@ -5,6 +5,11 @@ const _ = require("lodash");
 const getSpreadsheet = (spreadsheetId, credentials) =>
   new Promise((resolve, reject) => {
     const doc = new GoogleSpreadsheet(spreadsheetId);
+
+    if (!credentials) {
+      return resolve(doc);
+    }
+    
     doc.useServiceAccountAuth(credentials, function(err) {
       if (err) reject(err);
       else resolve(doc);
